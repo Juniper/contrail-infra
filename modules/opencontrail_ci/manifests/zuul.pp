d754738e972da8931a966dcf67fb28cf1d590886
Krzysztof Klimonda <krzysztof.klimonda@codilime.com>
2017-08-23 12:14:07 +0200

    create a minimal opencontrail_ci::zuul class that can be used to manage zuul server and layout.yaml

---
+++ modules/opencontrail_ci/manifests/zuul.pp
@@ -0,0 +1,24 @@
+class opencontrail_ci::zuul(
+  $layout_dir = '/etc/project-config/zuul',
+){
+
+  class { '::project_config':
+    url => 'https://github.com/kklimonda/contrail-project-config',
+  }
+
+  exec { 'zuul-reload':
+    command     => '/etc/init.d/zuul reload',
+    require     => File['/etc/init.d/zuul'],
+    refreshonly => true,
+  }
+
+  file { '/etc/zuul/layout/':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    recurse => true,
+    require => $::project_config::config_dir,
+  }
+
+}
