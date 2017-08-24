2efd740aa4a74d12059e87846944a1835f67bf33
Krzysztof Klimonda <krzysztof.klimonda@codilime.com>
2017-08-24 07:14:18 +0200

    Tweak PuppetDB firewall to limit access.

--- modules/opencontrail_ci/manifests/puppetdb.pp
+++ modules/opencontrail_ci/manifests/puppetdb.pp
@@ -5,6 +5,7 @@ class opencontrail_ci::puppetdb inherits opencontrail_ci::params {
 
   firewall { '100 accept port 8081 from puppet master':
     dport  => 8081,
+    source => $::opencontrail_ci::params::hosts['puppetmaster'],
     proto  => 'tcp',
     action => 'accept',
   }
