0e36c3faad9d252b5bbc30745fa752f7976352c3
Lukasz Lukasiewicz <l.m.lukasiewicz@gmail.com>
2017-09-22 00:40:12 +0200

    Add zuul-{server,merger,scheduler}

--- manifests/site.pp
+++ manifests/site.pp
@@ -87,8 +87,64 @@ node /^zl\d+\.opencontrail\.org$/ {
   }
 }
 
-node 'zuul2.opencontrail.org' {
-  class { '::opencontrail_ci::zuul': }
+node 'zuulv3.opencontrail.org' {
+  $gerrit_server        = 'review2.opencontrail.org'
+  # TODO: create user zuul3 in Gerrit and upload the key
+  $gerrit_user          = 'zuul3'
+  $gerrit_ssh_host_key  = hiera('gerrit_ssh_rsa_pubkey')
+  # TODO: create keypair for zuul
+  $zuul_ssh_private_key = hiera('zuul_ssh_private_key')
+  $zuul_url             = "http://${::fqdn}/p"
+  $git_email            = 'zuul@opencontrail.org'
+  $git_name             = 'OpenContrail Zuul'
+  # This is current name of openstack-infra/zuul branch.
+  $revision             = 'feature/zuulv3'
+
+  class { '::project_config':
+    url => 'https://git.openstack.org/openstack-infra/project-config',
+  }
+
+  # NOTE(pabelanger): We call ::zuul directly, so we can override all in one
+  # settings.
+  # XXX: It seems there is not a lot of things here
+  # to abstract to opencontrail_ci module. Need to revisit.
+  class { '::zuul':
+    gerrit_server                => $gerrit_server,
+    gerrit_user                  => $gerrit_user,
+    zuul_ssh_private_key         => $zuul_ssh_private_key,
+    git_email                    => $git_email,
+    git_name                     => $git_name,
+    revision                     => $revision,
+    python_version               => 3,
+    zookeeper_hosts              => 'nodepool.opencontrail.org:2181',
+    zuulv3                       => true,
+    connections                  => hiera('zuul_connections', []),
+    connection_secrets           => hiera('zuul_connection_secrets', []),
+    zuul_status_url              => 'http://127.0.0.1:8001/opencontrail',
+
+    # TODO: test with self-gen certs before rolling out production.
+    #gearman_client_ssl_cert      => hiera('gearman_client_ssl_cert'),
+    #gearman_client_ssl_key       => hiera('gearman_client_ssl_key'),
+    #gearman_server_ssl_cert      => hiera('gearman_server_ssl_cert'),
+    #gearman_server_ssl_key       => hiera('gearman_server_ssl_key'),
+    #gearman_ssl_ca               => hiera('gearman_ssl_ca'),
+    #proxy_ssl_cert_file_contents => hiera('zuul_ssl_cert_file_contents'),
+    #proxy_ssl_key_file_contents  => hiera('zuul_ssl_key_file_contents'),
+
+    proxy_ssl_cert_file_contents => hiera('proxy_ssl_cert_file_contents'),
+    proxy_ssl_key_file_contents  => hiera('proxy_ssl_key_file_contents'),
+  }
+
+  class { '::zuul::scheduler':
+    layout_dir     => $::project_config::zuul_layout_dir,
+    require        => $::project_config::config_dir,
+    python_version => 3,
+    # TODO: enable mysql or not?
+    use_mysql      => false,
+  }
+
+  class { '::zuul::web': }
+  class { '::zuul::server': }
 }
 
 node 'nodepool.opencontrail.org' {
