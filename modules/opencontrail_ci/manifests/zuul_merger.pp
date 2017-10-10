061a3aa08950e8b75abd8d26533345d35293c848
Krzysztof Klimonda <krzysztof.klimonda@codilime.com>
2017-10-10 11:38:16 +0200

    Update opencontrail_ci::zuul_merger to use hiera for ::zuul variables

--- modules/opencontrail_ci/manifests/zuul_merger.pp
+++ modules/opencontrail_ci/manifests/zuul_merger.pp
@@ -1,16 +1,6 @@
-class opencontrail_ci::zuul_merger(
-  $gearman_server = '127.0.0.1',
-) inherits opencontrail_ci::params {
+class opencontrail_ci::zuul_merger inherits opencontrail_ci::params {
 
-  class { '::zuul':
-    gearman_server       => $gearman_server,
-    gerrit_server        => 'review2.opencontrail.org',
-    gerrit_user          => 'zuulv3',
-    zuul_ssh_private_key => hiera('zuul_ssh_private_key'),
-    connections          => hiera('zuul_connections', []),
-    git_email            => 'zuul@opencontrail.org',
-    git_name             => 'OpenContrail Zuul',
-  }
+  class { '::zuul': }
 
   class { '::zuul::merger': }
 
