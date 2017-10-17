8dc1e313c2e3b086817081b1ab898c1d081eb1e2
Jarek Łukow <jaroslaw.lukow@codilime.com>
2017-10-17 14:38:01 +0200

    add a class for opening up gearman port for clients

---
+++ modules/opencontrail_ci/manifests/gearman_allow_client.pp
@@ -0,0 +1,8 @@
+define opencontrail_ci::gearman_allow_client {
+  firewall { "300 allow gearman connection for ${title}":
+    proto  => 'tcp',
+    dport  => hiera('::zuul::gearman_listen_port', 4730),
+    source => $title,
+    action => 'accept',
+  }
+}
