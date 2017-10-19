6b8f0251e49ea4a609156e0cd5cac8e8ffdb3153
Jarek Łukow <jaroslaw.lukow@codilime.com>
2017-10-19 10:04:51 +0200

    node

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,6 +34,26 @@ node /nl\d+(-dev|-jnpr)?.opencontrail.org/ {
   class { '::opencontrail_ci::nodepool_launcher': }
 }
 
+node 'nl03-dev.opencontrail.org' {
+  class { '::opencontrail_ci::server': }
+  class { '::opencontrail_ci::zuul_executor': }
+  class { '::opencontrail_ci::zuul_scheduler': }
+  class { '::opencontrail_ci::nodepool_launcher': }
+  class { '::opencontrail_ci::zuul_merger': }
+  # this is here because the zookeeper address is fixed in project-config repo and we want to
+  # point this nodepool it to the zuulv3-dev.o.o server here
+  host { 'zookeeper_host_entry':
+    ensure => present,
+    name   => 'nodepool.opencontrail.org',
+    ip     => '148.251.5.89',
+  }
+  host { 'gearman_host_entry':
+    ensure => present,
+    name   => 'zuulv3-dev.opencontrail.org',
+    ip     => '148.251.5.89',
+  }
+}
+
 node /nb\d+(-dev|-jnpr)?.opencontrail.org/ {
   class { '::opencontrail_ci::server': }
   class { '::opencontrail_ci::nodepool_builder': }
