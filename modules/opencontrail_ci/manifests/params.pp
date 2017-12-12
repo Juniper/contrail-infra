6b6d582c5be265f285a9d326a98d286c0e513f11
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2017-12-12 15:12:03 +0100

    Merge pulp deployment from development to production (#100)
    * Pulp repository (#98)
    Deployment of Pulp repository
    
    * Update common.yaml

--- modules/opencontrail_ci/manifests/params.pp
+++ modules/opencontrail_ci/manifests/params.pp
@@ -2,8 +2,18 @@ class opencontrail_ci::params {
   $cloud_credentials        = hiera('opencontrail_ci::cloud_credentials', {})
   $hosts                    = hiera('opencontrail_ci::hosts')
   $project_config_repo      = hiera('opencontrail_ci::project_config_repo')
+  $vim_package              = $::osfamily ? {
+    'Debian' => 'vim-nox',
+    'RedHat' => 'vim-enhanced',
+    default => 'vim',
+  }
+  $dnsutils_package         = $::osfamily ? {
+    'Debian' => 'dnsutils',
+    'RedHat' => 'bind-utils',
+    default => 'dnsutils',
+  }
   $common_packages          = [
     'curl', 'atop', 'tcpdump', 'unzip', 'strace',
-    'dnsutils', 'vim-nox', 'sysstat', 'iotop'
+    'sysstat', 'iotop', $vim_package, $dnsutils_package
   ]
 }
---
+++ modules/opencontrail_ci/manifests/pulp_repo.pp
@@ -0,0 +1,47 @@
+class opencontrail_ci::pulp_repo(
+  $pulp_version,
+) inherits opencontrail_ci::params {
+
+  include ::epel
+
+  yumrepo { "pulp-${pulp_version}-stable":
+    baseurl  => "https://repos.fedorapeople.org/repos/pulp/pulp/stable/${pulp_version}/\$releasever/\$basearch/",
+    descr    => "Pulp ${pulp_version} Production Releases",
+    enabled  => true,
+    gpgcheck => true,
+    gpgkey   => "https://repos.fedorapeople.org/repos/pulp/pulp/GPG-RPM-KEY-pulp-${pulp_version}",
+  }
+
+  class { '::pulp':
+    require       => Class['epel'],
+    enable_docker => true,
+    enable_rpm    => true,
+    ssl_username  => false,
+  }
+
+  class { '::pulp::admin':
+    ca_path => '/etc/pki/pulp/ca.crt'
+  }
+
+  pulp_rpmrepo { 'opencontrail-tpc':
+    ensure        => present,
+    display_name  => 'opencontrail-tpc',
+    description   => 'Third party packages required for OpenContail build',
+    relative_url  => 'opencontrail-tpc',
+    serve_http    => true,
+    serve_https   => true,
+    checksum_type => 'sha256',
+  }
+
+  firewall { '100 accept all to 80 - repos over http ':
+    proto  => 'tcp',
+    dport  => '80',
+    action => 'accept',
+  }
+
+  firewall { '100 accept all to 443 - repos over https + Pulp API ':
+    proto  => 'tcp',
+    dport  => '443',
+    action => 'accept',
+  }
+}
