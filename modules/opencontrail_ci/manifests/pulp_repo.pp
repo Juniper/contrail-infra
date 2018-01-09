cb68cb763dd280af9ea808b1e4842f432ca4b109
Lukasz Lukasiewicz <lukasz.lukasiewicz@codilime.com>
2018-01-09 12:23:27 +0100

    Add centos74 updates, extras and epel mirrors

--- modules/opencontrail_ci/manifests/pulp_repo.pp
+++ modules/opencontrail_ci/manifests/pulp_repo.pp
@@ -75,14 +75,52 @@ class opencontrail_ci::pulp_repo(
   }
 
   pulp_rpmrepo { 'centos74':
-    ensure        => present,
-    display_name  => 'centos74',
-    description   => 'englab centos74 mirror',
-    relative_url  => 'centos74',
-    serve_http    => true,
-    serve_https   => true,
-    checksum_type => 'sha256',
-    feed          => 'http://mirrors.mit.edu/centos/7/os/x86_64/',
+    ensure          => present,
+    skip            => 'distribution',
+    display_name    => 'centos74',
+    description     => 'englab centos74 mirror',
+    relative_url    => 'centos74',
+    serve_http      => true,
+    serve_https     => true,
+    checksum_type   => 'sha256',
+    feed            => 'http://mirrors.mit.edu/centos/7/os/x86_64/',
+    download_policy => 'background',
+  }
+
+  pulp_rpmrepo { 'centos74-updates':
+    ensure          => present,
+    display_name    => 'centos74-updates',
+    description     => 'englab centos74-updates mirror',
+    relative_url    => 'centos74-updates',
+    serve_http      => true,
+    serve_https     => true,
+    checksum_type   => 'sha256',
+    feed            => 'http://mirrors.mit.edu/centos/7/updates/x86_64/',
+    download_policy => 'background',
+  }
+
+  pulp_rpmrepo { 'centos74-extras':
+    ensure          => present,
+    display_name    => 'centos74-extras',
+    description     => 'englab centos74-extras mirror',
+    relative_url    => 'centos74-extras',
+    serve_http      => true,
+    serve_https     => true,
+    checksum_type   => 'sha256',
+    feed            => 'http://mirrors.mit.edu/centos/7/extras/x86_64/',
+    download_policy => 'background',
+  }
+
+  pulp_rpmrepo { 'centos74-epel':
+    ensure          => present,
+    display_name    => 'centos74-epel',
+    description     => 'englab centos74-epel mirror',
+    relative_url    => 'centos74-epel',
+    serve_http      => true,
+    serve_https     => true,
+    checksum_type   => 'sha256',
+    feed            => 'http://mirrors.mit.edu/epel/7/x86_64/',
+    download_policy => 'background',
   }
 
   selinux::port { 'crane':
