5402b6ace97d54144af4890a99914dc1b9c71dfe
Wojciech Urbański <wojciech.urbanski@codilime.com>
2018-04-05 17:44:18 +0200

    Change rhel variables' prefix to inject

--- modules/opencontrail_ci/manifests/nodepool_launcher.pp
+++ modules/opencontrail_ci/manifests/nodepool_launcher.pp
@@ -40,24 +40,7 @@ class opencontrail_ci::nodepool_launcher(
     require => File['/home/nodepool/.config'],
   }
 
-  file { '/opt/nodepool-inject-secrets.py':
-    source => 'puppet:///modules/opencontrail_ci/nodepool-inject-secrets.py',
-    owner  => 'root',
-    group  => 'root',
-  }
-
-  exec {'nodepool-inject-secrets':
-    command     => 'python /opt/nodepool-inject-secrets.py /etc/nodepool/nodepool.yaml.tmpl rhel-7 > /etc/nodepool/nodepool.yaml',
-    environment => [
-      "DINJ_REG_USER=${::nodepool::rhel_username}",
-      "DINJ_REG_PASSWORD=${::nodepool::rhel_password}",
-      "DINJ_REG_POOL_ID=${::nodepool::rhel_pool_id}",
-      ],
-    logoutput   => false,
-    require     => File['/opt/nodepool-inject-secrets.py'],
-  }
-
-  file { '/etc/nodepool/nodepool.yaml.tmpl':
+  file { '/etc/nodepool/nodepool.yaml':
     ensure  => present,
     source  => $::project_config::nodepool_config_file,
     owner   => 'nodepool',
