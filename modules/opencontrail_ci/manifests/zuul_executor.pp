2ef1f6b4530233338abcf3066cc7175c9c4f9f16
Lukasz Lukasiewicz <lukasz.lukasiewicz@codilime.com>
2018-04-13 13:17:14 +0200

    Add 3306 firewall exception for executors

--- modules/opencontrail_ci/manifests/zuul_executor.pp
+++ modules/opencontrail_ci/manifests/zuul_executor.pp
@@ -2,6 +2,12 @@ class opencontrail_ci::zuul_executor inherits opencontrail_ci::params {
 
   include ::zuul::known_hosts
 
+  firewall { '100 accept all to 3306 - build number db':
+    proto  => 'tcp',
+    dport  => '3306',
+    action => 'accept',
+  }
+
   class { '::project_config':
     url      => $::opencontrail_ci::params::project_config_repo,
     revision => 'master',
